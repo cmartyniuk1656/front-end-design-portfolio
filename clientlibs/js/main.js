bd324121f57aab9b1b5bf08b0c5b545026ebd67d
Chris Martyniuk <cmartyniuk1@gmail.com>
2019-05-04 10:27:24 -0400

    Projects updated, testing form submission

--- clientlibs/js/main.js
+++ clientlibs/js/main.js
@@ -1,13 +1,13 @@
 $(function() {
     
-    //Page load nav/header animation
+    //Page load nav/header animation -------------------------------------------------->
     setTimeout(function(){
     $('.content').addClass('active');
     $('.logo').addClass('active');
     }, 750);
     
     
-    //Add event listeners for deep link scrolling
+    //Add event listeners for deep link scrolling ------------------------------------->
     $("#info-scroll").click(function() {
         $('html,body').animate({
             scrollTop: $("#section-2").offset().top -125}, 'slow');
@@ -28,16 +28,67 @@ $(function() {
             scrollTop: $("#section-5").offset().top}, 'slow');
     });
     
-    //Add event listeners for project hidden content expansion
+    //Add event listeners for project hidden content expansion ------------------------->
     
-//    $("#pathfound").click(function() {
-//        if ($('#pathfound-additional').hasClass('expanded')) {
-//            console.info('class found');
-//        }
-//        else {
-//            console.info('class not found');
-//        }
-//    });
+    $("#view-pf-features").click(function() {
+        if (!$('#pathfound-features').hasClass('expanded')) {
+            $('#pathfound-active-video').removeClass('expanded');
+            $('#pathfound-features').addClass('expanded');
+        }
+    });
+    
+    $("#pf-create-account").click(function() {
+        pfExpandContent();
+        setVideoSrc('assets/videos/interacting-with-gameroom.mp4');
+    });
+    
+    $("#pf-create-join").click(function() {
+        pfExpandContent();
+        setVideoSrc('assets/videos/create-join-game.mp4');
+    });
+    
+    $("#pf-create-import").click(function() {
+        pfExpandContent();
+        setVideoSrc('assets/videos/create-import-character.mp4');
+    });
+    
+    $("#pf-gameroom").click(function() {
+        pfExpandContent();
+        setVideoSrc('assets/videos/interacting-with-gameroom.mp4');
+    });
+    
+    $("#pf-uploading").click(function() {
+        pfExpandContent();
+        setVideoSrc('assets/videos/uploading-assets.mp4');
+    });
     
 });
 
+// Contact form submit ------------------------------------------------------------------>
+
+
+$("#contact-submit").click(function() { 
+    
+    var name = $('#contact #name').val();
+    var email = $('#contact #email').val();
+    var message = $('#contact textarea').val();
+    console.info('mailto:cmartyniuk1@gmail.com?subject=The subject - ' + name + ' (' + email + ')' + '&body=' + message);
+    window.open('mailto:cmartyniuk1@gmail.com?subject=The subject - ' + name + ' (' + email + ')' + '&body=' + message + '_self');
+
+});
+
+
+// Utility functions -------------------------------------------------------------------->
+
+function pfExpandContent() {
+    if (!$('#pathfound-active-video').hasClass('expanded')) {
+            $('#pathfound-active-video').addClass('expanded');
+            $('#pathfound-features').removeClass('expanded');
+    }
+}
+
+function setVideoSrc(srcStr) {
+    if ($('#pathfound-active-video').attr('src') !== srcStr) {
+            $('#pathfound-active-video').attr('src', srcStr);
+        }
+}
